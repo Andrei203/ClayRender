076f9ac3a7949d5075475851137412cf4f7e1e2f
Andrei <andrei2.barlea@live.uwe.ac.uk>
2022-03-22 17:42:00 +0000

    rotating objects

---
+++ Assets/rotatesc.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rotatesc : MonoBehaviour
+{
+   [SerializeField] private Vector3 cubeRotate;
+   [SerializeField] private float rotateSpeed;
+
+    void Update()
+   {
+        transform.Rotate(cubeRotate * rotateSpeed * Time.deltaTime);  
+   }
+}
+
